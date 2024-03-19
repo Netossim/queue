f00f0d9642d14191c2c84c394cc235a57aff9104
Netossim <120585455+Netossim@users.noreply.github.com>
2024-03-19 16:23:40 -0300

    Update Aluguel.java

--- Aluguel.java
+++ Aluguel.java
@@ -1,18 +1,39 @@
 package Biblioteca;
-public class Aluguel extends Livro{
-    Usuario usuario;
-    Livro livro;
-    int dataAluguel;
-    int dataFinal; 
-    Float multa;
 
+import java.util.Date;
 
-    public String prazo;{
+public class Aluguel {
+    private Usuario usuario;
+    private Livro livro;
+    private Date dataAluguel;
+    private Date dataFinal; 
+    private float multaDiaria;
+    private float multaTotal;
 
-    };
+    // Construtor
+    public Aluguel(Usuario usuario, Livro livro, Date dataAluguel, Date dataFinal) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataAluguel = dataAluguel;
+        this.dataFinal = dataFinal;
+        this.multaDiaria = 5.0f; 
+        this.multaTotal = 0.0f;
+    }
 
+    public void calcularMulta() {
+        Date hoje = new Date(); 
+        if (hoje.after(dataFinal)) {
+            long diferencaMillis = hoje.getTime() - dataFinal.getTime();
+            long diferencaDias = diferencaMillis / (1000 * 60 * 60 * 24);
 
+            multaTotal = diferencaDias * multaDiaria;
+        } else {
+            multaTotal = 0.0f;
+        }
+    }
 
-
+    public float getMultaTotal() {
+        return multaTotal;
+    }
 
 }
